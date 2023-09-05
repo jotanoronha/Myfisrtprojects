0c4ef6f10721312b6c90047faff0e3844b0455c5
jotanoronha <143476157+jotanoronha@users.noreply.github.com>
2023-09-05 20:24:17 -0300

    Add files via upload

---
+++ Inputs and prints.py
@@ -0,0 +1,17 @@
+print ("L" in " Lira ")
+qtde_vendas = 1800
+print (qtde_vendas)
+qtde_lucro = 1500
+resultado = qtde_vendas - qtde_lucro
+print (resultado)
+# Mas aqui no caso o Usuário só vai escrever a resposta, porém não será possivel armazenar a informação
+
+input ("Qual seu nome ?" + " ")
+
+# Para conseguir armazenar uma informação usando o INPUT Faz-se :
+# Variavel = input(" Texto inserido do usuário")
+# Portanto Fica-se
+
+nome = input("Qual é seu nome ?" +" ")
+print (nome)
+
